b1b8a44e450d199464979e8525cef501e366c688
liu <1301100836@qq.com>
2016-08-01 16:18:04 +0800

    增加显示天气的功能

--- app/src/main/java/cn/studyjams/s1/sj130/ljrcoolweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/cn/studyjams/s1/sj130/ljrcoolweather/activity/ChooseAreaActivity.java
@@ -69,14 +69,14 @@ public class ChooseAreaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
-//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//        if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity) {
-//            Intent intent = new Intent(this, WeatherActivity.class);
-//            startActivity(intent);
-//            finish();
-//            return;
-//        }
+        isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
@@ -96,9 +96,9 @@ public class ChooseAreaActivity extends AppCompatActivity {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String countyCode = countyList.get(index).getCountyCode();
-//                    Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
-//                    intent.putExtra("county_code", countyCode);
-//                    startActivity(intent);
+                    Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+                    intent.putExtra("county_code", countyCode);
+                    startActivity(intent);
                     finish();
                 }
             }
@@ -252,8 +252,8 @@ public class ChooseAreaActivity extends AppCompatActivity {
             queryProvinces();
         } else {
             if (isFromWeatherActivity) {
-//                Intent intent = new Intent(this, WeatherActivity.class);
-//                startActivity(intent);
+                Intent intent = new Intent(this, WeatherActivity.class);
+                startActivity(intent);
             }
             finish();
         }
